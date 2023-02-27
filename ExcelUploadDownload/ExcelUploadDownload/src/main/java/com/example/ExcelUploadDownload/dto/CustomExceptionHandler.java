d37e7f5e791b9fc95984da0b04c2bb19eeea3778
Manikandan <89195312+manikandan531@users.noreply.github.com>
2023-02-27 22:06:18 +0530

    Add files via upload

---
+++ ExcelUploadDownload/ExcelUploadDownload/src/main/java/com/example/ExcelUploadDownload/dto/CustomExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.example.ExcelUploadDownload.dto;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+
+
+
+@ControllerAdvice
+public class CustomExceptionHandler 
+{
+	
+	
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public ResponseEntity<?> handleMaxSizeError()
+	{
+		return ErrorHandler.response(ErrorHandler.FILE_SIZE_EXCEEDS, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<?> requestedIdNotFound()
+	{
+		return ErrorHandler.response(ErrorHandler.FILE_ID_EMPTY, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MissingServletRequestPartException.class)
+	public ResponseEntity<?> requestFilenameNotFound()
+	{
+		return ErrorHandler.response(ErrorHandler.FILE_NAME_EMPTY, HttpStatus.NOT_FOUND);
+	}
+}
