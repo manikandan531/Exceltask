56f1a4cf6ca4cbf215a46c12e8a4c6b864429178
Manikandan <89195312+manikandan531@users.noreply.github.com>
2023-03-02 19:15:54 +0530

    Add files via upload

---
+++ ExcelUploadDownload/ExcelUploadDownload/src/main/java/com/example/ExcelUploadDownload/dto/CustomFileExtension.java
@@ -0,0 +1,9 @@
+package com.example.ExcelUploadDownload.dto;
+
+public class CustomFileExtension 
+{
+	public static final String extension1="ms-excel";
+	
+	public static final String extension2="sheet";
+	
+}
